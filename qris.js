7e43bb710af3d491b016b976876863ef2b906028
rembostore10 <rembostore10@gmail.com>
2026-04-14 10:01:30 +0700

    Update qris.js

--- qris.js
+++ qris.js
@@ -11,7 +11,7 @@ const QRIS_LINKS = {
   20000:'https://qris.example.com/pay/20000',
   25000:'https://qris.example.com/pay/25000',
   26500:'https://qris.example.com/pay/26500',
-  28000:'https://files.catbox.moe/5prwgi.png',
+  28000:'https://i.ibb.co.com/39KP4hkn/IMG-20260413-151641.jpg',
   35000:'https://qris.example.com/pay/35000',
   38000:'https://qris.example.com/pay/38000',
   40000:'https://qris.example.com/pay/40000',

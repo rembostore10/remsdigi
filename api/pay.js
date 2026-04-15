0529df3869e22bec787e325096f5d467c1dee8c7
rembostore10 <rembostore10@gmail.com>
2026-04-15 17:10:06 +0700

    Update pay.js

--- api/pay.js
+++ api/pay.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
   const authString = Buffer.from(serverKey + ":").toString('base64');
 
   try {
-    const response = await fetch('https://app.midtrans.com/snap/v1/transactions', {
+    const response = await fetch('https://app.sandbox.midtrans.com/snap/v1/transactions', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
